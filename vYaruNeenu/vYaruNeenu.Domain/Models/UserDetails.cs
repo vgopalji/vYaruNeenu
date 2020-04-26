ecc3a3a3f7dc857d0870021d0f46acfb29590182
VENUGOPAL S MUDDAHANUMAIAH <venugopal.muddahanumaiah@conduent.com>
2020-04-26 17:02:08 +0530

    Adding USerDetails Model to Domain Project

---
+++ vYaruNeenu/vYaruNeenu.Domain/Models/UserDetails.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace vYaruNeenu.Domain.Models
+{
+    public class UserDetails
+    {
+        string id { set; get; }
+        string name { set; get; }
+        string email { set; get; }
+        string mobileNumber { set; get; }
+    }
+}
